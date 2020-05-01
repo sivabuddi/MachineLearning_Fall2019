e5edf28a2901184983a4abdd7b49b9f0031f8b94
sivakumar <sivakumar.umkc.fall2019@gmail.com>
2020-05-01 15:20:40 -0500

    clean_outputs.py

---
+++ LAB_EXAM_QUESTIONS/Solutions/language_processing/clean_outputs.py
@@ -0,0 +1,10 @@
+'''
+Code to remove the files generated and re-generate them again.
+'''
+
+import os
+
+os.remove("lem_output.txt")
+os.remove("token_output.txt")
+os.remove("trigrams_output.txt")
+os.remove("trigram_repetition_output.txt")
