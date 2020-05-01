4411cf00565cad38daf17f8bc1ad29310f5855d2
sivakumar <sivakumar.umkc.fall2019@gmail.com>
2020-05-01 14:41:22 -0500

    main

---
+++ ML_icp1/main.py
@@ -0,0 +1,8 @@
+from stringreplacement import  Replace
+from string_reverse import  Reverse
+
+stringreplace =  Replace()
+print(stringreplace.replace("Im Playing with pythons","python","Pythons"))
+
+sreverse = Reverse()
+print(sreverse.reverse_stip_characters("sivakumarbuddi",5))
